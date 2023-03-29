6fe8d56884bfb20089f79258429a04b3c857e9cb
Dmitriy Anikeyev <anikeyev@ap-team.ru>
2023-03-30 02:34:02 +0600

    lastCommitUpdate

---
+++ DKey.CodeForces/Problem1780G/Solver1780G.cs
@@ -0,0 +1,11 @@
+﻿namespace DKey.CodeForcesExamples.Problem1780G;
+
+/// <summary>
+/// https://codeforces.com/contest/1780/problem/G
+/// </summary>
+public static class Solver1780G
+{
+    public static void Solve()
+    {
+    }
+}
