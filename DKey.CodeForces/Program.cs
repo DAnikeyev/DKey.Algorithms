cb60d29f1088e0cdb885c0a1521c3ef04b13c6ca
Dmitriy Anikeyev <anikeyev@ap-team.ru>
2023-04-04 20:31:26 +0600

    typo

--- DKey.CodeForces/Program.cs
+++ DKey.CodeForces/Program.cs
@@ -1,11 +1,11 @@
 ﻿using DKey.Algorithms;
-using DKey.CodeForces.Contest1805;
+using DKey.CodeForces.Contests1811;
 
 namespace DKey.CodeForces;
 
 public static class Program
 {
-    private static Solver _solver = new Solver1805D();
+    private static Solver _solver = new Solver1811A();
     public static void Main()
     {
         _solver.Run();
